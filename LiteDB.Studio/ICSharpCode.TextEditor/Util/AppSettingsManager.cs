315b3574fccf9dcdd2154476234707fafb472913
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-07-19 00:07:41 +0200

    Init AppSettingsManager static class
    This Class should manage application settings
    All theses settings should saved in app settings
    
    see: https://docs.microsoft.com/en-us/dotnet/framework/winforms/advanced/how-to-write-user-settings-at-run-time-with-csharp

---
+++ LiteDB.Studio/ICSharpCode.TextEditor/Util/AppSettingsManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB.Studio.ICSharpCode.TextEditor.Util
+{
+    public static class AppSettingsManager
+    {
+        public static bool IsLoadLastDbEnabled()
+        {
+            return Properties.Settings.Default.LoadLastDb;
+        }
+
+        public static void SetLoadLastDb(bool enable)
+        {
+            Properties.Settings.Default.LoadLastDb = enable;
+            Properties.Settings.Default.Save();
+        }
+
+        public static void SetLastDbPath(string path)
+        {
+            Properties.Settings.Default.LastDbPath = path;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
