61d44d2d9bcb3aaf7aa636088adb3a9d240e7b9e
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-07-28 15:51:27 +0200

    Create ApplicationSettings Model
    This Mode initialized with two fields LastConnectionStrings,LoadLastDbOnStartup

---
+++ LiteDB.Studio/ICSharpCode.TextEditor/Model/ApplicationSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace ICSharpCode.TextEditor.Util.Model
+{
+    [Serializable]
+    public class ApplicationSettings
+    {
+        public ConnectionString LastConnectionStrings { get; set; }
+        public bool LoadLastDbOnStartup { get; set; }
+
+        public ApplicationSettings()
+        {
+        }
+    }
+}
