c76b2d773ff7e76daa2eaf830823f0eb35da1f6e
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-07-28 15:54:48 +0200

    Implement OnExit of The Application and persist AppSettingsManager changes

--- LiteDB.Studio/Program.cs
+++ LiteDB.Studio/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ICSharpCode.TextEditor.Util;
 
 namespace LiteDB.Studio
 {
@@ -14,9 +15,16 @@ namespace LiteDB.Studio
         [STAThread]
         static void Main(string[] args)
         {
+            Application.ApplicationExit += OnExit;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm(args.Length == 0 ? null : args[0]));
+        }
+
+        private static void OnExit(object sender, EventArgs eventArgs)
+        {
+            Application.ApplicationExit -= OnExit;
+            AppSettingsManager.PersistData();
         }
     }
 }
